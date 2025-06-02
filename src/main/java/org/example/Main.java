205e19835dd598bb1633bb282b976ba75b5f8513
Om357 <om.rathod2004.or@gmail.com>
2025-06-02 18:18:29 +0530

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main   {
+    public static void main(String[] args) {
+
+        World world = new World();
+        world.start();
+
+        for( ; ; ){ // INFINITE LOOP
+        System.out.println("Hello ");
+    }
+    }
+}
---
+++ src/main/java/org/example/World.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class World extends Thread{
+
+    @Override
+    public void run() {
+        for( ; ; ){
+            System.out.println("World!");
+        }
+    }
+}
